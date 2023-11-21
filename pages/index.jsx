bc892e46721a311898e56e177b9f0c4f25c7a6a8
HisamNasir <85251630+HisamNasir@users.noreply.github.com>
2023-11-21 15:57:39 +0500

    done

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,24 +1,45 @@
 import React, { useState } from 'react';
 import PDFViewer from '../components/PDFViewer';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import Dropzone from 'react-dropzone';
+import Box from '@mui/material/Box';
 
 const Home = () => {
   const [selectedFile, setSelectedFile] = useState(null);
 
-  const handleFileChange = (e) => {
-    const file = e.target.files[0];
-    if (file && file.type === 'application/pdf') {
-      setSelectedFile(URL.createObjectURL(file));
+  const handleDrop = (acceptedFiles) => {
+    if (acceptedFiles.length > 0 && acceptedFiles[0].type === 'application/pdf') {
+      setSelectedFile(URL.createObjectURL(acceptedFiles[0]));
     } else {
       setSelectedFile(null);
     }
   };
 
   return (
-    <div>
-      <h1>PDF Viewer</h1>
-      <input type="file" accept=".pdf" onChange={handleFileChange} />
+    <Container>
+      <Typography variant="h1" gutterBottom>
+        PDF Viewer
+      </Typography>
+      <Dropzone onDrop={handleDrop} accept=".pdf">
+        {({ getRootProps, getInputProps }) => (
+          <Box
+            {...getRootProps()}
+            sx={{
+              border: '2px dashed #ccc',
+              borderRadius: '4px',
+              padding: '20px',
+              textAlign: 'center',
+              cursor: 'pointer',
+            }}
+          >
+            <input {...getInputProps()} />
+            <Typography>Drag and drop a PDF file here, or click to select one.</Typography>
+          </Box>
+        )}
+      </Dropzone>
       {selectedFile && <PDFViewer pdfFile={selectedFile} />}
-    </div>
+    </Container>
   );
 };
 
